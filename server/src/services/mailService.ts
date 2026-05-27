0259dd474b983e9598f14dbbd77fb8663c5c8122
Sibasis Routray <sibasis.routray@quotus.co.in>
2026-05-27 15:40:42 +0530

    implemented forget password

--- server/src/services/mailService.ts
+++ server/src/services/mailService.ts
@@ -1,4 +1,5 @@
 import nodemailer from 'nodemailer';
+import SMTPTransport from 'nodemailer/lib/smtp-transport';
 
 if (!process.env.EMAIL_USER || !process.env.EMAIL_PASS) {
   console.error('ERROR: EMAIL_USER or EMAIL_PASS not found in environment variables. Email services will fail.');
@@ -7,12 +8,14 @@ if (!process.env.EMAIL_USER || !process.env.EMAIL_PASS) {
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST || 'smtp.gmail.com',
   port: parseInt(process.env.EMAIL_PORT || '587'),
-  secure: process.env.EMAIL_SECURE === 'true', // true for 465, false for other ports
+  secure: process.env.EMAIL_SECURE === 'true',
+  family: 4,
+
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: process.env.EMAIL_USER!,
+    pass: process.env.EMAIL_PASS!,
   },
-});
+} as SMTPTransport.Options);
 
 export const sendResetPasswordEmail = async (email: string, resetToken: string) => {
   const resetUrl = `${process.env.FRONTEND_URL}/reset-password?token=${resetToken}`;
