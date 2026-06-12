0e88115b82c3dbc6567acdc4efd5ca33113c33e8
Sibasis Routray <sibasis.routray@quotus.co.in>
2026-06-12 17:06:53 +0530

    -added vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: 3003,
+    allowedHosts: [
+      'thehappyfoodcompany.com',
+      'www.thehappyfoodcompany.com'
+    ]
+  }
 })
+
